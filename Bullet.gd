eb1fea77e32a6a9fd1ad513a715ef36ba1d5d607
chlotod <cjane2709@gmail.com>
2021-02-17 21:13:24 -0500

    Completes the exercise

---
+++ Bullet.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+onready var VP = get_viewpoint().size
+onready var Explosions = get_node("/root/Game/Explosions")
+var Explosion = load("res://Explosion/Explosion.tcsn")
+
+export var Velocity = Vector2(0,-1)
+export var Acceleration = Vector2(0,-1) 
+
+
+func _physics_process(_delta):
+	position += Velocity 
+	Velocity += Acceleration 
+	if global_position.x < -10 or global_position.x > VP.x + 10 or global_position.y < -10 or global_position.y > VP.y + 10:
+		 queue_free()
+
+
+
+
+func _on_Bullet_body_entered(body):
+	var explosion = Explosion.instance()
+	explosion.global_position =  global_position
+	Explosions.add_child(explosion)
+	body.queue_free()
+	queue_free()
